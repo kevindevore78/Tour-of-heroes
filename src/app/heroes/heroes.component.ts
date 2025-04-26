b19b45adc53c2d5f02f914ae9f894dafcc2bc053
Kevin Devore <kevindevore78@gmail.com>
2025-04-25 21:17:15 -0400

    three

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import {HEROES} from '../mock-heroes';
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
+@Component({
+  standalone: true,
+  imports: [
+    NgFor,
+  ],
+})
 
 @Component({
   selector: 'app-heroes',
@@ -10,9 +21,7 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'};
+ 
     heroes = HEROES;
 
 }
